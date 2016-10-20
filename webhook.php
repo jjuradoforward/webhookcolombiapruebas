6bf474b9ca34fd33d97d88afa25d8c871a6c6299
jjuradoforward <forward.hubdataanalyst@gmail.com>
2016-10-20 12:46:13 -0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -10,3 +10,8 @@ if ($verify_token === 'abc123') {
 
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
+
+use FacebookAds\Object\Lead;
+
+$form = new Lead(<LEAD_ID>);
+$form->read();
